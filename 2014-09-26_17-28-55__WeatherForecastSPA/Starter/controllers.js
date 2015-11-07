cd648b0c648e6d847bfeeca30f6a3a67e275388f
phits <john@fitz.com>
2015-11-07 13:09:48 -0500

    Completed course.

--- 2014-09-26_17-28-55__WeatherForecastSPA/Starter/controllers.js
+++ 2014-09-26_17-28-55__WeatherForecastSPA/Starter/controllers.js
@@ -1,42 +1,35 @@
-weatherApp.controller('homeController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location', '$resource', '$routeParams', 'cityService', function($scope, $location, $resource, $routeParams, cityService) {
     
     $scope.city = cityService.city;
     
-    $scope.$watch('city', function() {
-        
+    $scope.$watch('city', function() {      
         cityService.city = $scope.city;
-        
     })
+    
+    $scope.submit = function() {
+        $location.path('/forecast');
+    };
     
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', function($scope, $routeParams, cityService, weatherService) {
     
     $scope.city = cityService.city;
     
     $scope.days = $routeParams.days || '2';
     
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast", { 
-        callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
     
-    $scope.weatherResult = $scope.weatherAPI.get({appid: "2ab327be8eaf8e007c2dcf3c976cb681", q: $scope.city, cnt: $scope.days });
-    
-   $scope.convertToFahrenheight = function(degK) {
-       
-       return Math.round((1.8 * (degK - 273)) + 32);
-       
-   }
+    $scope.convertToFahrenheight = function(degK) { 
+       return Math.round((1.8 * (degK - 273)) + 32); 
+    }
    
-   $scope.convertToDate = function(dt) {
-       
-       return new Date(dt * 1000);
-       
-   }
+    $scope.convertToDate = function(dt) {  
+       return new Date(dt * 1000);  
+    }
     
-    $scope.$watch('city', function() {
-        
-        cityService.city = $scope.city;
-        
+    $scope.$watch('city', function() { 
+        cityService.city = $scope.city;   
     })
     
 }]);
--- 2014-09-26_17-28-55__WeatherForecastSPA/Starter/services.js
+++ 2014-09-26_17-28-55__WeatherForecastSPA/Starter/services.js
@@ -1,6 +1,17 @@
 // Services
-weatherApp.service('cityService', function() {
-    
+weatherApp.service('cityService', function() {   
     this.city = 'New York, NY';
-    
 });
+
+weatherApp.service('weatherService', [ '$resource', function($resource) {
+    
+    this.GetWeather = function(city, days) {
+        
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast", { 
+        callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+        return weatherAPI.get({appid: "2ab327be8eaf8e007c2dcf3c976cb681", q: city, cnt: days });
+        
+    }
+    
+}]);
